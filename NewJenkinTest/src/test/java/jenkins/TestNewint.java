0127b3a25eaaf0cd613946b66fd69be715744b35
Vandana2016 <vandana.c.singh@accenture.com>
2016-10-24 13:18:13 +0530

    Update TestNewint.java

--- NewJenkinTest/src/test/java/jenkins/TestNewint.java
+++ NewJenkinTest/src/test/java/jenkins/TestNewint.java
@@ -16,7 +16,7 @@ public class TestNewint {
 	//WebDriver driver = new ChromeDriver();
 	DesiredCapabilities Capability=new DesiredCapabilities();	
 	DesiredCapabilities Capability=DesiredCapabilities.chrome();
-	DesiredCapabilities Capability.setPlatform(org.openqa.selenium.Platform.ANY);
+	Capability.setPlatform(org.openqa.selenium.Platform.ANY);
 	WebDriver driver=new RemoteWebDriver(Capability); 
 	
 	@BeforeClass
