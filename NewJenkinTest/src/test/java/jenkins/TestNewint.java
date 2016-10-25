1acd30a1f263ac31b111fdcba6c38e196f1f94e2
Vandana2016 <vandana.c.singh@accenture.com>
2016-10-25 11:31:55 +0530

    Update TestNewint.java

--- NewJenkinTest/src/test/java/jenkins/TestNewint.java
+++ NewJenkinTest/src/test/java/jenkins/TestNewint.java
@@ -27,7 +27,7 @@ public class TestNewint {
 	new DesiredCapabilities();
        // URL serverurl = new URL("http://localhost:9515");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        WebDriver driver = new RemoteWebDriver(System.setProperty("webdriver.chrome.driver","\\home\\vandana\\ChromeDriver\\chromedriver"),capabilities);
+        WebDriver driver = new RemoteWebDriver(System.setProperty("webdriver.chrome.driver","\\home\\vandana\\Jenkins\\NewJenkinTest\\chromedriver"),capabilities);
         driver.get("http://www.google.com");
         WebElement searchEdit = driver.findElement(By.name("q"));
         searchEdit.sendKeys("Selftechy on google");
