8f097a723f0ee841fb3d4e1338e115e9ce11564c
Vandana2016 <vandana.c.singh@accenture.com>
2016-10-26 16:15:00 +0530

    Update Sing.java

--- NewJenkinTest/src/test/java/jenkins/Sing.java
+++ NewJenkinTest/src/test/java/jenkins/Sing.java
@@ -1,9 +1,19 @@
 package jenkins;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class Sing {
 	
 	public void singing(){
 		System.out.println("I'm singing");
 	}
-
+	public static WebDriver test(){
+		new DesiredCapabilities();
+		DesiredCapabilities Capability=DesiredCapabilities.chrome();
+		Capability.setPlatform(org.openqa.selenium.Platform.ANY);
+		WebDriver driver=new ChromeDriver(Capability);
+		return driver;
+	}
 }
